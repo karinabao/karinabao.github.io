75073aa0d8b1a6002bd7eb12d285927fe408307d
karinabao <39714767+karinabao@users.noreply.github.com>
2020-08-30 17:48:58 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { GlobalStyles } from './global-styles';
-import { App } from './app';
-import { FirebaseContext } from './context/firebase';
-
-const config = {
-  apiKey: 'AIzaSyDAU60M2FDHNsYt9uGMpRezw3FGVIX6Jrs',
-  authDomain: 'netflix-c8ae9.firebaseapp.com',
-  databaseURL: 'https://netflix-c8ae9.firebaseio.com',
-  projectId: 'netflix-c8ae9',
-  storageBucket: 'netflix-c8ae9.appspot.com',
-  messagingSenderId: '590575805931',
-  appId: '1:590575805931:web:f809092b6cd55306fa25ae',
-};
-
-const firebase = window.firebase.initializeApp(config);
-
-ReactDOM.render(
-    <FirebaseContext.Provider value={{ firebase: window.firebase }}>
-        <GlobalStyles />
-        <App />
-    </FirebaseContext.Provider>, 
-    document.getElementById('root')
-);
+export { default as Accordion } from './accordion';
+export { default as Jumbotron } from './jumbotron';
+export { default as Footer } from './footer';
+export { default as OptForm } from './opt-form';
+export { default as Header } from './header';
+export { default as Feature } from './feature';
+export { default as Form } from './form';
+export { default as Profiles } from './profiles';
